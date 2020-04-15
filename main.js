7899bc29e90b495d706c63717391929a8988bd55
Patrick <patrick.hurley@codeimmersives.com>
2020-04-15 15:02:06 -0400

    finished

--- main.js
+++ main.js
@@ -1,7 +1,62 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const Grade = function (assignment, score) {
+  return {
+    assignment: assignment,
+    score: score,
+  };
+};
 
+const Term = function (hours) {
+  return {
+    hours: hours,
+    grades: [],
+    addGrade: function (assignment, score) {
+      this.grades.push(Grade(assignment, score));
+    }
+  };
+};
+
+const Course = function (name) {
+  return {
+    name: name,
+    terms: [],
+    addTerm: function (hours) {
+      this.terms.push(Term(hours));
+    }
+  };
+};
+
+const Student = function (name, course, term = 1) {
+  return {
+    name: name,
+    course: course,
+    term: term,
+    courses: [],
+    addCourse: function (name) {
+      this.courses.push(Course(name));
+    },
+
+    getAverage: function (courseName, termIndex = (this.term - 1)) {
+      for (let i = 0; i < this.courses.length; i++) {
+        if (this.courses[i].name === courseName) {
+          let total = 0;
+          for (let k = 0; k < this.courses[i].terms[termIndex].grades.length; k++) {
+            total += this.courses[i].terms[termIndex].grades[k].score;
+          }
+          return total / this.courses[i].terms[0].grades.length;
+        } else if (courseName === undefined && this.courses[i].name === this.course) {
+          let total = 0;
+          for (let k = 0; k < this.courses[i].terms[termIndex].grades.length; k++) {
+            total += this.courses[i].terms[termIndex].grades[k].score;
+          }
+          return total / this.courses[i].terms[0].grades.length;
+        }
+      }
+    }
+  };
+};
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
@@ -29,4 +84,4 @@ module.exports = {
   Term,
   Course,
   Student,
-}
+};
